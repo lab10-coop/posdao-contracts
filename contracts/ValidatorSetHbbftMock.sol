d0f770c1ffa4e794f3a83e884d9772aa6c1e5ac9
davidf <dforsten@gmail.com>
2019-11-10 15:41:07 +0100

    Storing public keys in the validator set mock

--- contracts/ValidatorSetHbbftMock.sol
+++ contracts/ValidatorSetHbbftMock.sol
@@ -1,18 +1,23 @@
 pragma solidity 0.5.9;
+pragma experimental ABIEncoderV2;
 
 import './ValidatorSetHbbft.sol';
 
 /// @dev mock contract
 contract ValidatorSetHbbftMock is ValidatorSetHbbft {
 
+    mapping(address => bytes32[2]) public validatorPubkeys;
 
     function finalizeChange() external onlySystem {
         _currentValidators = _pendingValidators;
         delete _pendingValidators;
     }
 
-    constructor(address[] memory _validators) public {
+    constructor(address[] memory _validators, bytes32[2][] memory _pubkeys) public {
         _currentValidators = _validators;
+        for (uint256 i = 0; i < _validators.length; i++) {
+            validatorPubkeys[_validators[i]] = _pubkeys[i];
+        }
     }
 
     // =============================================== Setters ========================================================
