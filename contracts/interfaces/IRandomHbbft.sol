3468bc7cb073e9a431e4a950025cb6580ada6fe1
didi <git@d10r.net>
2020-02-16 19:52:40 +0100

    removed hbbft stuff - it's being developed elsewhere

--- contracts/interfaces/IRandomHbbft.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity 0.5.10;
-
-
-interface IRandomHbbft {
-    function initialize(uint256, address) external;
-    function currentSeed() external view returns(uint256);
-}
--- contracts/interfaces/IValidatorSetHbbft.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity 0.5.10;
-
-
-interface IValidatorSetHbbft {
-    function blockRewardContract() external view returns(address);
-}
